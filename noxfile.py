fb6a068f0254789193bda849e0f9b90ea3cdcd82
burgholzer <burgholzer@me.com>
2024-01-15 18:14:50 +0100

    🔧 updated nox job for docs

--- noxfile.py
+++ noxfile.py
@@ -2,20 +2,40 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+import argparse
 
 import nox
 
-if TYPE_CHECKING:
-    from nox.sessions import Session
-
 
 @nox.session(reuse_venv=True)
-def docs(session: Session) -> None:
-    """Build the documentation.
+def docs(session: nox.Session) -> None:
+    """Build the docs. Pass "--serve" to serve. Pass "-b linkcheck" to check links."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--serve", action="store_true", help="Serve after building")
+    parser.add_argument("-b", dest="builder", default="html", help="Build target (default: html)")
+    args, posargs = parser.parse_known_args(session.posargs)
 
-    Simply execute `nox -s docs` to locally build and serve the docs.
-    """
+    if args.builder != "html" and args.serve:
+        session.error("Must not specify non-HTML builder with --serve")
+
+    extra_installs = ["sphinx-autobuild"] if args.serve else []
     session.install("-r", "docs/requirements.txt")
     session.chdir("docs")
-    session.run("sphinx-autobuild", "-b", "html", ".", "_build/html")
+
+    if args.builder == "linkcheck":
+        session.run("sphinx-build", "-b", "linkcheck", ".", "_build/linkcheck", *posargs)
+        return
+
+    shared_args = (
+        "-n",  # nitpicky mode
+        "-T",  # full tracebacks
+        f"-b={args.builder}",
+        ".",
+        f"_build/{args.builder}",
+        *posargs,
+    )
+
+    if args.serve:
+        session.run("sphinx-autobuild", *shared_args)
+    else:
+        session.run("sphinx-build", "--keep-going", *shared_args)
